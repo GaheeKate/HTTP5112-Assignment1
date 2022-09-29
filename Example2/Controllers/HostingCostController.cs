16a0379c8cff6d92b0b6c6a76a9baecd0f83d052
Gahee Kate Choi <gahee189@gmail.com>
2022-09-29 12:32:26 -0400

    Updated var types and added taxdiscount function to match with the instruction PDF.

--- Example2/Controllers/HostingCostController.cs
+++ Example2/Controllers/HostingCostController.cs
@@ -12,9 +12,10 @@ namespace Example2.Controllers
 
         /// <summary>
         /// This method returns the hosting cost. Hosting cost is $5.50 / FN (fortnight = 14 days), 
-        /// plus an additional 13% HST
+        /// plus an additional 13% HST. 
         /// </summary>
-        /// <returns> The web hosting cost by fortnight, 13% HST, Total cost
+        /// <returns> The web hosting cost by fortnight, 
+        /// 13% HST if tax is greater than $2, tax will be rounded down to two decimal places, and Total cost.
         /// </returns>
         /// <param name="id">The number of days which has elapsed sicne the beginning of the hosting.</param>
         /// <example>
@@ -22,18 +23,34 @@ namespace Example2.Controllers
         /// "2 fortnights at $5.50/FN = $11.00 CAD"
         /// "HST 13% = $1.43 CAD"
         /// "Total = $12.43 CAD"
+        /// GET: api/HostingCost/28 ->
+        /// "2 fortnights at $5.50/FN = $11.00 CAD"
+        /// "HST 13% = $2.14 CAD"
+        /// "Total = $18.64 CAD"
         /// </example>
 
         public IEnumerable<string> Get(int id)
         {
-            var Fortnights = (((id + 1) / 14) + 1);
-            var Cost = Fortnights * 5.50;
-            var Tax = Cost * 0.13;
-            var Total = Cost + Tax;
+            int Fortnights = (((id + 1) / 14) + 1);
+            double Cost = Fortnights * 5.50;
+            double Tax = Cost * 0.13;
+            double Taxdiscounted = Tax;
+            double Total;
+
+
+
+            if (Tax > 2)
+            {
+                Taxdiscounted = Tax - 0.005;
+            }
+
+            Total = Cost + Taxdiscounted;
+
             return new string[] {Fortnights + " fortnights at $5.50/FN = $"+ string.Format("{0:0.00}",Cost)+ " CAD",
-                   "HST 13% = $" + string.Format("{0:0.00}",Tax) + " CAD",
+                   "HST 13% = $" + string.Format("{0:0.00}",Taxdiscounted) + " CAD",
                    "Total = $" + string.Format("{0:0.00}",Total) + " CAD"};
         }
+
 
     }
 }
